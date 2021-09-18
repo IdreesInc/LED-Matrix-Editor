be0a8f9e369b39a2b30c281e0d5ee9d3ecfa9310
Idrees Hassan <idrees@idreesinc.com>
2021-09-18 17:59:11 -0400

    Send frame height on resize

--- application.js
+++ application.js
@@ -137,6 +137,9 @@ function resizePalette() {
     document.documentElement.style.setProperty("--circle-diameter", width * 0.40 + "px");
     colorWheel.wheelDiameter = width;
     colorWheel.redraw();
+    if (inIframe()) {
+        window.top.postMessage("frame-height: " + window.document.body.scrollHeight, "*");
+    }
 }
 
 function drawAtCoordinates(x, y) {
